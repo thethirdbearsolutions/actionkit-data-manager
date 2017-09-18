52096d34e2d19114b060ee4a415b2779e5df0208
Ethan Jucovy <ethan.jucovy@gmail.com>
2017-09-18 12:45:29 +0000

    Add job types for deleting arbitrary actions (+associated donations) and arbitrary user modifications

--- basic_tasks/forms.py
+++ basic_tasks/forms.py
@@ -243,6 +243,99 @@ The SQL must return a column named `actionfield_id`. New Actionfield Name is opt
 
         return n_rows, n_success, n_error
 
+class ActionDeleteJobForm(BatchForm):
+    help_text = """The SQL must return a column named `action_id`.  All of these actions will be really, permanently, deleted. Be careful."""
+
+    def run(self, task, rows):
+        rest = RestClient()
+        rest.safety_net = False
+
+        n_rows = n_success = n_error = 0
+
+        task_log = get_task_log()
+        for row in rows:
+            task_log.sql_log(task, row)
+            n_rows += 1
+            assert row.get("action_id") and int(row['action_id'])
+
+            resp = rest.action.delete(id=row['action_id'])
+            if resp is not None:
+                n_error += 1
+                task_log.error_log(task, resp)
+            else:
+                n_success += 1
+
+        return n_rows, n_success, n_error
+
+class UserModificationForm(BatchForm):
+
+    actionkit_page = forms.CharField(label="Optional tracking page name",
+                                     required=False)
+
+    help_text = """
+The SQL must return a column named `user_id`.
+
+All columns with prefix `new_data_` will be treated as new values
+for the core_user attributes.  For example, `select user_id, "United Kingdom"
+as new_data_country, "London" as new_data_city, country, city from core_user
+where id in (100,101,102,103);` would cause four user records to have their
+country and city attributes set to "United Kingdom" and "London" respectively.
+
+Columns prefixed new_data_user_* can also be used to set or update userfield
+values.
+
+All columns apart from user_id and new_data_* will be ignored by the job code
+(but can be used to review records for accuracy, log old values, etc)
+"""
+    def run(self, task, rows):
+        rest = RestClient()
+        rest.safety_net = False
+
+        task_log = get_task_log()
+
+        n_rows = n_success = n_error = 0
+
+        for row in rows:
+            task_log.sql_log(task, row)
+            n_rows += 1
+
+            assert row.get("user_id") and int(row['user_id'])
+
+            new_values = {"id": row['user_id']}
+            for key in row:
+                if not key.startswith("new_data_"):
+                    continue
+                new_values[key.replace("new_data_", "", 1)] = row[key]
+
+            task_log.activity_log(task, new_values)
+            new_values.pop("id")
+            try:
+                rest.user.put(id=row['user_id'], **new_values)
+                resp = {}
+                resp['log_id'] = row['user_id']
+
+                if self.cleaned_data.get("actionkit_page"):
+                    page = self.cleaned_data['actionkit_page']
+                    data = {"id": row['user_id'], "page": page}
+                    for field in row:
+                        if field.startswith("action_"):
+                            data[field] = row[field]
+                    api = Client()
+                    tracking_resp = api.act(data)
+                    resp['tracking_action'] = tracking_resp
+                task_log.success_log(task, resp)
+            except Exception, e:
+                n_error += 1
+                resp = {}
+                resp['log_id'] = row['user_id']
+                resp['error'] = traceback.format_exc()
+                task_log.error_log(task, resp)
+            else:
+                n_success += 1
+
+        return n_rows, n_success, n_error
+
+
 class UnsubscribeAndActJobForm(BatchForm):
     help_text = """
 <p>The SQL must return a column named `user_id`.</p>
--- basic_tasks/models.py
+++ basic_tasks/models.py
@@ -1,9 +1,13 @@
 from main.task_registry import register_task
-from basic_tasks.forms import (UnsubscribeAndActJobForm,
-                               ActionfieldRenameJobForm,
-                               UserfieldJobForm,
-                               PublishReportResultsForm,
-                               ActionkitSpreadsheetForm)
+from basic_tasks.forms import (
+    UnsubscribeAndActJobForm,
+    ActionfieldRenameJobForm,
+    UserfieldJobForm,
+    PublishReportResultsForm,
+    ActionkitSpreadsheetForm,
+    ActionDeleteJobForm,
+    UserModificationForm,
+)
 
 register_task("UserfieldJob", 
               "Apply Userfield to Batch", UserfieldJobForm)
@@ -16,3 +20,5 @@ register_task("PublishReportResultsJob",
               "Run and Publish Reports", PublishReportResultsForm)
 register_task("ActionkitSpreadsheetJob", 
               "Stream sql results to a Google spreadsheet", ActionkitSpreadsheetForm)
+register_task("ActionDeleteJob", "Delete Actions", ActionDeleteJobForm)
+register_task("UserModificationJob", "Modify Users", UserModificationForm)
