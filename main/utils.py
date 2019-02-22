f7cc06b470548d4412406a6adf892a6184cd69c4
Ethan Jucovy <ethan.jucovy@gmail.com>
2019-02-22 15:48:27 +0000

    more flexible flush util

--- main/utils.py
+++ main/utils.py
@@ -2,9 +2,10 @@ import datetime
 from django.utils import timezone
 from .models import JobTask
 
-def flush_old_tasks():
-    one_month_ago = timezone.now() - datetime.timedelta(days=30)
+def flush_old_tasks(days_ago=30):
+    one_month_ago = timezone.now() - datetime.timedelta(days=days_ago)
     jobs = JobTask.objects.filter(
         created_on__lte=one_month_ago
     )
+    print 'Deleting %s jobs' % jobs.count()
     jobs.delete()
